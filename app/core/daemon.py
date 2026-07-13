cca61f0e386c0086efc8ba1e3d59c65c37864c67
NickyHariniaina <ferrissushi@gmail.com>
2026-07-13 23:56:54 +0300

    feat: implement fetching stopped service

--- app/core/daemon.py
+++ app/core/daemon.py
@@ -8,22 +8,49 @@ Service use services.msc and linux use systemctl
 It is harder to use manage service on windows. That's why this backend is here.
 """
 
+TYPES_MAP = {
+    win32service.SERVICE_WIN32_OWN_PROCESS: "Own Process",
+    win32service.SERVICE_WIN32_SHARE_PROCESS: "Share Process",
+    win32service.SERVICE_KERNEL_DRIVER: "Kernel Driver",
+    win32service.SERVICE_FILE_SYSTEM_DRIVER: "File System Driver"
+}
+
+def open_SCM_with_all_access():
+    return win32service.OpenSCManager(None, None, win32service.SC_MANAGER_ALL_ACCESS)
+
+def query(scm, service_type, service_status):
+    return win32service.EnumServicesStatusEx(scm, service_type, service_status)
+
+def get_stopped_services():
+    scm = open_SCM_with_all_access()
+
+    # Is there a better way to fetch 3 type at the same type?
+    # I should create an array and then mix them later.
+    raw = query(scm, win32service.SERVICE_WIN32, win32service.SERVICE_STOPPED)
+    win32service.CloseServiceHandle(scm)
+
+    services = []
+    for svc in raw:
+        services.append({
+            "name": svc["ServiceName"],
+            "display_name": svc["DisplayName"],
+            "status": "Stopped",
+            "service_type": TYPES_MAP.get(svc["ServiceType"], "Other")
+        })
+
+    services.sort(key=lambda service: service["name"].lower())
+    return services
+
 def get_running_services():
     # SCM = Service Control Manager: central database that manages all services.
     # OpenSCManager usually takes three params (machine name, database name, persmission level)
     # The default if we put None for both are (local computer, SCM database, ...)
     # Later I want to give full access so I may need to refactor this win32Service so I can
-    # use the SC_MANAGER_ALL_ACCESS.
-    scm = win32service.OpenSCManager(
-        None, None, win32service.SC_MANAGER_ENUMERATE_SERVICE
-    )
+    # use the SC_MANAGER_ALL_ACCESS. *DONE
+
+    scm = open_SCM_with_all_access()
     
-    # Main service query.
-    raw = win32service.EnumServicesStatusEx(
-        scm,
-        win32service.SERVICE_WIN32,
-        win32service.SERVICE_ACTIVE,
-    )
+    raw = query(scm, win32service.SERVICE_WIN32, win32service.SERVICE_ACTIVE)
 
     win32service.CloseServiceHandle(scm)
 
@@ -38,22 +65,14 @@ def get_running_services():
     # I was thinking of not showing Kernel related daemon but whatever... for now let's put everything.
     # FYI: they are multiple of 2 to easily do bitwise operation on multiple service types?
     # Need to check a clear documentatino of this code for later
-    # For a better approach, I'll use pywin32's built-in constant
-    types_map = {
-        win32service.SERVICE_WIN32_OWN_PROCESS: "Own Process",
-        win32service.SERVICE_WIN32_SHARE_PROCESS: "Share Process",
-        win32service.SERVICE_KERNEL_DRIVER: "Kernel Driver",
-        # Showing kernel driver service is useless, since we cannot interact with it because it load and unload with the full system
-        win32service.SERVICE_FILE_SYSTEM_DRIVER: "File System Driver"
-    }
 
     services = []
     for svc in raw:
         services.append({
             "name": svc["ServiceName"],
             "display_name": svc["DisplayName"],
-            "status": "Running", # we need this since the frontend needs it.
-            "service_type": types_map.get(svc["ServiceType"], "Other")
+            "status": "Running",
+            "service_type": TYPES_MAP.get(svc["ServiceType"], "Other")
         })
 
     services.sort(key=lambda service: service["name"].lower())
